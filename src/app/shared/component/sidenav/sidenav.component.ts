a50b824d928df1c8de8d9e1dc9954cfd8a5da7ea
renan <renanboniza@email.com>
2024-05-23 00:47:27 +0800

    profile sidenav

--- src/app/shared/component/sidenav/sidenav.component.ts
+++ src/app/shared/component/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SidenavService } from './_service/sidenav.service';
+import { SecureStorageService } from 'src/app/login/_Auth/secure-storage.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent {
+      constructor(private sidenavService:SidenavService,
+                  private secureStorageService: SecureStorageService
+      ){}
+
+
+
+  ngOnInit(){
+    this.employee();
+  }
+
+  employee() {
+    const timesheetIdString = this.secureStorageService.getItem("userId");
+  
+    this.sidenavService.getSectionsLeaderboard().subscribe((res: any) => {
+      const ds = res.data;
+  
+      const userArray = ds.filter((user: any) => user.id === timesheetIdString);
+  
+      if (userArray.length > 0) {
+        const userRole = userArray[0].role;
+
+        let dashboardLink = '';
+        let leaderboardLink = '';
+        if (userRole === 'teacher') {
+          dashboardLink = '/teacher-dashboard';
+          leaderboardLink = '/teacher-leaderboard';
+        } else if (userRole === 'headTeacher') {
+          dashboardLink = '/head-dashboard';
+          leaderboardLink = '/head-leaderboard';
+        }
+  
+        // Set the side navigation links
+        this.setSideNavLink('dashboardLink', dashboardLink);
+        this.setSideNavLink('leaderboardLink', leaderboardLink);
+  
+        
+      }
+    });
+  }
+  
+  
+
+  setSideNavLink(linkId: string, link: string) {
+    const sideNavElement = document.getElementById(linkId);     
+  if (sideNavElement) {
+    sideNavElement.setAttribute('href', link);
+  }
+}
+
 
 }
